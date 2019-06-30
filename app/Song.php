2d197cd4cc7fd537e063fae5927577e9026e325b
datatype <neelkanthkaushik2014@gmail.com>
2019-06-30 12:50:01 +0530

    Api Version 2

--- app/Song.php
+++ app/Song.php
@@ -4,7 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Song extends Model
-{
-    //
+class Song extends Model {
+
+    /**
+     * Modify model object
+     * @return $this
+     */
+    public function modify() {
+        $this->duration = round($this->duration / 60, 2); //change the duration from seconds to minutes
+        return $this;
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Http\Request;
   |
  */
 
+//Api Version 1 routes
 Route::group(['namespace' => 'Api\V1', 'prefix' => 'v1', 'as' => 'api.v1.'], function () {
+    Route::get('songs', 'SongsController@index')->name('songs.index');
+    Route::get('songs/{id}', 'SongsController@show')->name('songs.show')->where('id', '[0-9]+');
+});
+//Api Version 2 routes
+Route::group(['namespace' => 'Api\V2', 'prefix' => 'v2', 'as' => 'api.v2.'], function () {
     Route::get('songs', 'SongsController@index')->name('songs.index');
     Route::get('songs/{id}', 'SongsController@show')->name('songs.show')->where('id', '[0-9]+');
 });
