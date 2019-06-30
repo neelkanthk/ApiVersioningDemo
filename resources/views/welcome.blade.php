7409aede3dc606a6df3aa9a3edb2ea812d18c6d2
datatype <neelkanthkaushik2014@gmail.com>
2019-06-30 20:40:34 +0530

    Modified homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,96 +4,49 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Api Versioning Demo</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            <h1><a href="https://www.datatype.in/restful-api-versioning-using-inheritance">Version your RESTful APIs like a pro through Inheritance</a></h1>
+            <h4>Version 1 APIs</h4>
+            <p>1. All songs</p> GET {{route('api.v1.songs.index')}}
+            <p>2. Song by id</p> GET {{route('api.v1.songs.show',1)}}
+            <p>3. Update a song by id</p> PATCH {{route('api.v1.songs.update',1)}}
+            <pre>
+            Request Header: Content-Type: application/json
+            Request Body:
+            {
+                "title": "Faded",
+                "artist": "Alan Walker"
+            }
+            </pre>
+            <h4>Version 2 APIs</h4>
+            <p>1. All songs</p> GET {{route('api.v2.songs.index')}}
+            <p>2. Song by id</p> GET {{route('api.v2.songs.show',1)}}
+            <p>3. Update a song by id</p> PATCH {{route('api.v2.songs.update',1)}}
+            <pre>
+            Request Header: Content-Type: application/json
+            Request Body:
+            {
+                "title": "Faded",
+                "artist": "Alan Walker"
+            }
+            </pre>
+            <p>3. Add a new song</p> POST {{route('api.v2.songs.store')}}
+            <pre>
+            Request Header: Content-Type: application/json
+            Request Body:
+            {
+                "title": "Hymn for the weekend",
+                "artist": "Coldplay",
+                "genre": "pop",
+                "duration": 500
+            }
+            </pre>
         </div>
     </body>
 </html>
